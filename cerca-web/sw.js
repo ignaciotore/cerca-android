13e8bf45ee5ccaccb9a6c254c110d535724076ec
ignaciotore <ignacio.tore.deymonnaz@gmail.com>
2026-09-21 20:38:50 -0300

    Actualizar PWA con Inicio simplificado

--- cerca-web/sw.js
+++ cerca-web/sw.js
@@ -1,4 +1,4 @@
-const CACHE='cerca-web-v10';
+const CACHE='cerca-web-v11';
 const STATIC=['/','/index.html','/styles.css','/app.js','/pwa.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',e=>{

a5045e556fefc5a6a0dfcfdd87e1946b306d5366
ignaciotore <ignacio.tore.deymonnaz@gmail.com>
2026-09-21 20:24:55 -0300

    Actualizar PWA con roles de contactos

--- cerca-web/sw.js
+++ cerca-web/sw.js
@@ -1,4 +1,4 @@
-const CACHE='cerca-web-v7';
+const CACHE='cerca-web-v8';
 const STATIC=['/','/index.html','/styles.css','/app.js','/pwa.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',e=>{

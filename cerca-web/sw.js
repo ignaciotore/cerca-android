b816726179dfdc13e6b27b79b6acb99addc8f5e2
ignaciotore <ignacio.tore.deymonnaz@gmail.com>
2026-09-21 20:18:28 -0300

    Actualizar caché PWA para suscripciones CERCA

--- cerca-web/sw.js
+++ cerca-web/sw.js
@@ -1,4 +1,4 @@
-const CACHE='cerca-web-v5';
+const CACHE='cerca-web-v6';
 const STATIC=['/','/index.html','/styles.css','/app.js','/pwa.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',e=>{

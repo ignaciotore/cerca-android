19d2befaeddeb509a9b32e9ea90267908214379f
ignaciotore <ignacio.tore.deymonnaz@gmail.com>
2026-09-21 20:33:03 -0300

    Actualizar PWA con carga de contactos desde la web

--- cerca-web/sw.js
+++ cerca-web/sw.js
@@ -1,4 +1,4 @@
-const CACHE='cerca-web-v9';
+const CACHE='cerca-web-v10';
 const STATIC=['/','/index.html','/styles.css','/app.js','/pwa.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',e=>{

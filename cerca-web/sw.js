c6f487cb8d6f23d3d5744c4bfad0f8439a0c5de4
ignaciotore <ignacio.tore.deymonnaz@gmail.com>
2026-09-23 13:07:25 -0300

    Forzar actualización de permisos de notificaciones en la PWA

--- cerca-web/sw.js
+++ cerca-web/sw.js
@@ -1,5 +1,5 @@
-const CACHE='cerca-web-v17';
-const STATIC=['/','/index.html','/styles.css','/app.js','/contact-role-fix.js','/notification-flow-fix.js','/pwa.js','/manifest.webmanifest','/icon.svg'];
+const CACHE='cerca-web-v18';
+const STATIC=['/','/index.html','/styles.css','/app.js','/contact-role-fix.js','/notification-flow-fix.js','/notification-permission.js','/pwa.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting()));
